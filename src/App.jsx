0c5d428e77fe9b5cc31387f00a10e672bd5e87ca
saikiran11461 <70562454+saikiran11461@users.noreply.github.com>
2022-05-13 19:43:09 +0530

    created people card list with react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Review from './Review';
+import "./App.css"
+function App() {
+  return <main>
+    <section className="container">
+      <div className="title">
+        <h2>our review</h2>
+        <div className="underline"></div>
+      </div>
+      <Review/>
+    </section>
+  </main>
+}
+
+export default App;
